cebc1137d0dfcf1729ea44d18db111a604a9e641
Lucien Rohwein Aibel <lucien.aibel@mymail.champlain.edu>
2023-12-05 17:45:26 -0500

    Added comments and cleaned up code

--- functions.cpp
+++ functions.cpp
@@ -8,49 +8,53 @@
 
 using namespace std;
 
-void print(string sentence)
+void print(string sentence) //Takes in a string and prints it out letter by letter. For effect.
 {
     for (int k = 0; k < sentence.size();k++)
     {
         cout << sentence[k];
-        Sleep(0);//210);
+        Sleep(210);
     }
 }
 
-int splitString(string str, string* out, char split)
+int splitString(string str, string* out, char split, short maxValues) //Splits a string into chunks seperated by some character 'split'
 {
-    stringstream ss;
-    short actionNum = 0;
-    for (int i = 0; i < str.size();i++)
+    stringstream ss; //Stream for storing each chunk as we build it
+    short chunkNum = 0;
+    for (int i = 0; i < str.size();i++) //Go through the string one character at a time
     {
-        if (str[i] != split)
+        if (str[i] != split) //If it's not the split character, add it to the current chunk
         {
             ss << str[i];
         }
-        if (str[i] == split or i == str.size() - 1)
+        if (str[i] == split or i == str.size() - 1) //If it is the split character, or this is the last character, save the current chunk
         {
-            out[actionNum] = ss.str();
+            out[chunkNum] = ss.str();
             ss.str("");
-            actionNum++;
+            chunkNum++;
         }
-        if (actionNum >= 10) //10 is the maximum size of the outside array. god this feels hacky.
+        //We need to both add the last character of the string (which shouldn't be a split character) and then save the last chunk.
+        //That's why we have the extra bit about the last character only in the saving block
+
+        //Error checking; make sure we're not overflowing the outside array.
+        if (chunkNum >= maxValues)
         {
-            invalid_argument("splitString is trying to create more than 10 values");
+            throw invalid_argument("splitString is trying to create more values than the passed array has space for");
         }
     }
-    return actionNum;
+    return chunkNum; //return the number of chunks found
 }
 
-string actionList(string actions[], int actionLen)
+string actionList(string actions[], int actionLen) //Prettys up a list of actions for display
 {
-    stringstream ss;
-    for (int i = 0; i < actionLen; i++)
+    stringstream ss; //Buffer to hold the output
+    for (int i = 0; i < actionLen; i++) //For each action
     {
-        ss << actions[i];
-        if (i < actionLen - 1) {
+        ss << actions[i]; //Add it to the output
+        if (i < actionLen - 1) { //Seperated by a comma if it's not the last element
             ss << ", ";
         }
-        if (i == actionLen - 2)
+        if (i == actionLen - 2) //Add an "or" to the split only if it's the second to last element
         {
             ss << "or ";
         }
--- main.cpp
+++ main.cpp
@@ -5,28 +5,28 @@
 
 using namespace std;
 
-const string EVENTS[] = {"Bear!","Calming down","AAAH","AAAAAAH"};
-const string PROMPTS[] = {"Your troop is approached by a bear.","","",""};
-const string ACTIONS[] = {"stay calm/get loud/scatter","","",""};
-const string NEXT_EVENTS[] = {"1/2/3","","",""};
+const string EVENTS[] = {"Bear!","Calming down","AAAH","AAAAAAH"}; //This stores the titles of the various events
+const string PROMPTS[] = {"Your troop is approached by a bear.","","",""}; //This stores the main description the user is shown
+const string ACTIONS[] = {"stay calm/get loud/scatter","","",""}; //This stores the list of actions available for them to take. The sublist is stored as a slash-seperated string
+const string NEXT_EVENTS[] = {"1/2/3","","",""}; //Choosing an action will move the user to another event. This stores the list of next events, also using a slash-seperated string. Each one results from the action in the same position - so if the action choices were run/hide, and the user types 'hide', the second slash-seperated number would be the event to go to.
 
-const short MAX_ACTIONS = 10;
-const string FILENAME = "adventurelog.txt";
+const short MAX_ACTIONS = 10; //Maximum number of actions for one event. Should be as small as possible to save memory.
+const string FILENAME = "adventurelog.txt"; //Target for writing down the logs
 
 int main(){
-    short currentEvent = 0;
+    short currentEvent = 0; //Keeps track of the current event the user is on
     ofstream fout (FILENAME);
 
-    while (currentEvent != -1)
+    while (currentEvent != -1) //Mainloop
     {
-        print(EVENTS[currentEvent] + "\n");
+        print(EVENTS[currentEvent] + "\n"); //Print the name and description of the event
         print(PROMPTS[currentEvent] + "\n");
 
-        string actions[MAX_ACTIONS];
-        int actionLen = splitString(ACTIONS[currentEvent],actions,'/');
+        string actions[MAX_ACTIONS]; //Find the list of valid actions the user can take
+        int actionLen = splitString(ACTIONS[currentEvent],actions,'/', MAX_ACTIONS);
 
         short choiceNum = -1;
-        while (choiceNum == -1)
+        while (choiceNum == -1) //Wait for the user to input a valid action
         {
             print("You can order the troop to " + actionList(actions,actionLen) + ".\n");
             cout << "\n";
@@ -34,7 +34,7 @@ int main(){
             string chosenAction;
             getline(cin, chosenAction);
             cout << "\n";
-            for (int i = 0; i < actionLen; i++)
+            for (int i = 0; i < actionLen; i++) //Check if the user input matches anything in the list of valid actions
             {
                 if (chosenAction == actions[i])
                 {
@@ -42,18 +42,19 @@ int main(){
                     break;
                 }
             }
-            if (choiceNum == -1)
+            if (choiceNum == -1) //If the user didn't choose a valid action, let them know
             {
                 print("You give the order, but the troop doesn't seem to understand. Maybe try something else?\n");
             }
         }
 
-        string nextEvents[MAX_ACTIONS];
-        int nextLen = splitString(NEXT_EVENTS[currentEvent],nextEvents,'/');
-        currentEvent = stoi(nextEvents[choiceNum]);
-    }
-    fout << "Welcome to the Adventure! " << endl;
+        string nextEvents[MAX_ACTIONS]; //Get the list of next events to go to
+        splitString(NEXT_EVENTS[currentEvent],nextEvents,'/',MAX_ACTIONS);
 
+        currentEvent = stoi(nextEvents[choiceNum]); //Set the current event to the event we're going to
+    }
+
+    //Wrap up
     fout.close();
     return 0;
 }
